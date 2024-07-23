845643ad9e9a486592b68cc9ca132c373d403c70
meghawarna7 <meghzpro@gmail.com>
2024-07-23 23:30:37 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "research"
+AUTHOR_USER_NAME = "meghawarna"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "meghzzpro@gmail.com"
